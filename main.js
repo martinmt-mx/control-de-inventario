92554728924c1abb56a021fbf95036c2e2ab159f
martinmt-mx <mmojica0@ucol.mx>
2020-10-21 23:42:49 -0500

    insertar no funciona aun

--- main.js
+++ main.js
@@ -46,15 +46,40 @@ class Almacen{
             }
         }
     }
+
+    listarProductosInverso(){
+        for(let i = this.capacidad.length-1; i>=0; i--){
+            if(this.capacidad[i] !== undefined){
+                console.log(`${i}: ${this.capacidad[i].producto}`)
+            }
+        }
+    }
+
+    insertarProducto(producto, posicion){
+        if(this.capacidad.length !== 20){
+            let sigProducto
+            let productoAct = producto
+            
+            for(let i = posicion; i<=this.capacidad.length; i++){
+                sigProducto = this.capacidad[i+1]
+                this.capacidad[i] = productoAct
+                productoAct = sigProducto
+            }
+        }
+    }
 }
 
 let producto1 = new Producto(1,"Arroz","Arrozito rico", 30, 10)
 let producto2 = new Producto(2,"Pan","Pan rico", 20, 5)
 let producto3 = new Producto(3,"Elote","Elote rico", 25, 8)
+let producto4 = new Producto(3,"Nito","Nito rico", 10, 10)
 let almacen = new Almacen()
 almacen.agregarProducto(producto1)
 almacen.agregarProducto(producto2)
 almacen.agregarProducto(producto3)
-console.log(almacen.borrarProductoID(3))
+console.log()
+almacen.insertarProducto(producto4)
 almacen.listarProductos()
+almacen.listarProductosInverso()
+
 console.log(almacen)
